e60da4bd40612d0d0472727e28184fa831f766c8
eginko <eginko@users.noreply.github.com>
2017-05-25 10:07:04 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 DEVICE=$1
-[ "$DEVICE" = "" ] && DEVICE=fenix3
+[ "$DEVICE" = "" ] && DEVICE=fenix5
 
 RESOURCE_PATH=$(find . -path './resources*.xml' | xargs | tr ' ' ':')
 monkeyc -o bin/activity-widget.prg -d $DEVICE -m manifest.xml -z $RESOURCE_PATH src/*.mc
